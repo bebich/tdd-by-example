06ded6d6a114de9b58029209b89ac4a4b53e9e03
fbebek <finassistapp@gmail.com>
2020-10-24 18:47:08 +0200

    Addition, finally

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -38,5 +38,9 @@ public class Money {
 
     public Money times(int multiplicator) {
         return new Money(amount*multiplicator, this.currency);
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
